528cfa7e7ded8485941c63a43253a68fbeb982bf
Logan Tegman <ltegman@gmail.com>
2016-01-16 11:52:11 -0800

    remove lint rules from gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
@@ -5,47 +7,20 @@ const mocha = require('gulp-mocha');
 var files = ['gulpfile.js', './lib/*.js', './test/test*.js',
  '!node_modules/**', '!*.json'];
 
-gulp.task('lint', function() {
+gulp.task('lint', () => {
   return gulp.src(files)
-  .pipe(eslint(
-    {
-      'rules': {
-        'indent': [
-          2,
-          2
-        ],
-        'quotes': [
-          2,
-          'single'
-        ],
-        'linebreak-style': [
-          2,
-          'unix'
-        ],
-        'semi': [
-          2,
-          'always'
-        ]
-      },
-      'env': {
-        'es6': true,
-        'node': true,
-        'mocha': true
-      },
-      'extends': 'eslint:recommended'
-    }
-  ))
+  .pipe(eslint())
   .pipe(eslint.format())
   .pipe(eslint.failAfterError());
 });
 
-gulp.task('mocha', function() {
+gulp.task('mocha', () => {
   return gulp.src('test/test*.js')
-  .pipe(mocha({reporter: 'nyan'}));
+  .pipe(mocha({ reporter: 'nyan' }));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', () => {
   return gulp.watch(files, ['lint', 'mocha']);
 });
 
-gulp.task('default', ['watch','lint', 'mocha']);
+gulp.task('default', ['watch', 'lint', 'mocha']);
